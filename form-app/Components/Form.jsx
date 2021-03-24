f36f3f4d9eba77e3bd180c3ab6544f718b376403
unknown <ricardo.baltazar04@gmail.com>
2021-03-24 13:49:00 -0300

    configurando projeto next e criando componente Form

---
+++ form-app/Components/Form.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Form(){
+    return (
+        <>
+            <h1>Ola Mundo!</h1>
+        </>
+    )
+};
---
+++ form-app/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Form from '../Components/Form'
+
+export default function Home() {
+  return (
+    <>
+      <Form />
+    </>
+  )
+}
